3e189528b6813f155c2cedca3d07d35e01f7cb9d
dav_pr <dav_pr@tutanota.com>
2022-11-09 22:41:15 +0200

    рішення задачі

--- Project1.py
+++ Project1.py
@@ -1,3 +1,11 @@
 # https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e
+
+import numpy as np
+# рішення моє особисте
 def sum_of_differences(arr):
-    return max(arr) - min(arr) if arr else 0
+
+    return np.diff(np.array(sorted(arr))).sum()
+
+if __name__ == "__main__":
+
+    print (sum_of_differences([-1, -9, 6, -12, 1, -4, 14, 10, 1, -13, 10]))
